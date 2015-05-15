1674de90db8dd8e33996a21fbb8823fa88a9cb1a
Carlos Rueda Morales <crueda@gmail.com>
2015-05-15 13:30:11 +0200

    con heading

--- fr24parse.py
+++ fr24parse.py
@@ -23,11 +23,11 @@ import uuid
 import urllib2, json
  
 data = urllib2.urlopen('http://krk.fr24.com/zones/fcgi/spain.json').read()
+#data = urllib2.urlopen('http://krk.fr24.com/zones/fcgi/na_ne_all.json').read()
+
+
 json_data = json.loads(data)
  
-#print json_data
-
-#print json_data['60162b0']
 featureVector = []
 for key, value in json_data.iteritems():
 	#print key
@@ -35,39 +35,31 @@ for key, value in json_data.iteritems():
 	try:
 		lat = value[1]
 		lon = value[2]
-		#print lat
+		heading = value[3]
 		vectorCoordinates = []
 		vectorCoordinates.append(lon)
 		vectorCoordinates.append(lat)
 		new_feature = {}
 		new_feature['type'] = 'Feature'
+		new_properties = {}
+		new_properties['name'] = key
+		new_properties['heading'] = heading
 		new_geometry = {}
 		new_geometry['type'] = 'Point'
 		new_geometry['coordinates'] = vectorCoordinates
 		new_feature['geometry'] = new_geometry
+		new_feature['properties'] = new_properties
 		featureVector.append(new_feature)
 	except:
-		#print "a"
 		pass
 
-new_data = {}
-new_data['type'] = 'FeatureCollection'
-new_data['features'] = featureVector
-geojson_data = json.dumps(new_data)
+#new_data = {}
+#new_data['type'] = 'FeatureCollection'
+#new_data['features'] = featureVector
+#geojson_data = json.dumps(new_data)
+#geojson_data = json.dumps(featureVector)
 
-print geojson_data
+with open('./html/aviones_all.json', 'w') as outfile:
+    #json.dump(new_data, outfile)
+    json.dump(featureVector, outfile)
 
-#for key, value in geojson_data.iteritems():
-#	print key
-#	print value
-
-'''
-import json
-json.dumps(['variable', {'lista': ('item1', None, 15.4, 20)}])
-print json.dumps({"w": 0, "b": 0, "r": 0}, sort_keys=True)
-lista={'a':1,'b':2,'c':3}
-json.dumps(lista)
-
-f = open('a.json', 'w')
-json.dump(x, f)
-'''
